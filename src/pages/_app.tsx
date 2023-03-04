b4b048c74e0cdb42ca654a5ee5cc3280597edd08
vzwork <vladislav.zakharov.work@gmail.com>
2023-03-04 15:48:26 -0800

    landing setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
 import type { AppProps } from 'next/app'
 
+// declare module '@mui/material/styles' {
+//   interface Palette {
+//     hello: Palette['background']
+//   }
+// }
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    palette: {
+      background: {
+        default: '#bdc7c9',
+      },
+      primary: {
+        main: '#3c783c',
+      },
+    },
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
